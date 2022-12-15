e64d972aaf52850c4f8d77d40792bf7fefac1d67
HHNebula <sungsoomr@icloud.com>
2022-12-15 12:33:19 +0900

    first commit

---
+++ src/test/java/com/spring/learn_servlet/LearnServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learn_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
